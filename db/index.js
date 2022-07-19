116cf7466d40376258b0c68c56d879593bd8dc21
Teo Berguerre <teothou@hotmail.fr>
2022-07-19 16:12:02 +0200

    force connection

--- db/index.js
+++ db/index.js
@@ -13,23 +13,23 @@ export default async function initDB(){
             database: process.env.DB_DB
         });                                     // the old one cannot be reused.
 
-        db.connect(function(err) {              // The server is either down
-            if(err) {                                     // or restarting (takes a while sometimes).
-                console.log('error when connecting to db:', err);
-                setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-            }                                     // to avoid a hot loop, and to allow our node script to
-        });
-        // process asynchronous requests in the meantime.
-                                                // If you're also serving http, display a 503 error.
-        db.on('error', function(err) {
-            console.log('db error', err);
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-                db.query('select 1 + 1', (err, rows) => { /* */ });// Connection to the MySQL server is usually
-                handleDisconnect();                         // lost due to either server restart, or a
-            } else {                                      // connnection idle timeout (the wait_timeout
-                throw err;                                  // server variable configures this)
-            }
-        });
+        // db.connect(function(err) {              // The server is either down
+        //     if(err) {                                     // or restarting (takes a while sometimes).
+        //         console.log('error when connecting to db:', err);
+        //         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        //     }                                     // to avoid a hot loop, and to allow our node script to
+        // });
+        // // process asynchronous requests in the meantime.
+        //                                         // If you're also serving http, display a 503 error.
+        // db.on('error', function(err) {
+        //     console.log('db error', err);
+        //     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        //         db.query('select 1 + 1', (err, rows) => { /* */ });// Connection to the MySQL server is usually
+        //         handleDisconnect();                         // lost due to either server restart, or a
+        //     } else {                                      // connnection idle timeout (the wait_timeout
+        //         throw err;                                  // server variable configures this)
+        //     }
+        // });
     }
 
     handleDisconnect();
