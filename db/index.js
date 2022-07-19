dead76d1abcf7a5b442dd60561952af4d1f01d45
Teo Berguerre <teothou@hotmail.fr>
2022-07-19 15:50:09 +0200

    db check connection lost

--- db/index.js
+++ db/index.js
@@ -4,18 +4,32 @@ dotenv.config();
 
 
 export default async function initDB(){
-    const db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DB
-    });
-    await db.connect((err) => {
-        if (err){
-            console.log(err)
-        }else {
-            console.log('connected to mysql')
-        }
-    });
+    let db;
+    function handleDisconnect() {
+         db = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DB
+        });                                     // the old one cannot be reused.
+
+        db.connect(function(err) {              // The server is either down
+            if(err) {                                     // or restarting (takes a while sometimes).
+                console.log('error when connecting to db:', err);
+                setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+            }                                     // to avoid a hot loop, and to allow our node script to
+        });                                     // process asynchronous requests in the meantime.
+                                                // If you're also serving http, display a 503 error.
+        db.on('error', function(err) {
+            console.log('db error', err);
+            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+                handleDisconnect();                         // lost due to either server restart, or a
+            } else {                                      // connnection idle timeout (the wait_timeout
+                throw err;                                  // server variable configures this)
+            }
+        });
+    }
+
+    handleDisconnect();
     return db;
 }
